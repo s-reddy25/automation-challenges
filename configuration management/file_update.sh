18304b230c393ad896d9dd32fae3799e88b4bd4d
sandersonjobs <sandersonjobs@gmail.com>
2015-05-28 14:50:28 -0400

    file_update.sh
    This script will update the template.file file with the appropriate value of `facter -p widget` from each remote server listed in server_update_list file. It keeps track of successes and failures and reports this back to you. The script will also return a list of failed IP addresses.

---
+++ configuration management/file_update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+###################################################################
+#   Script to update and distribute file
+#
+#   This script takes a file (server_update_list) as input. Place 
+#   this file, along with the tempplate.file in the same directory
+#   as this script.Create this file as a list of IP addresses 
+#   targeted for the updated file. The script will iterate throught 
+#   the list, retrieving the facter value from the remote server, 
+#   updating the file locally and then scp'ing the file to the remote machine.
+#   By: Michael Sanderson
+
+### Initialize success and fail variables to avoid printing a blank
+success=0
+fail=0
+###
+
+
+for server in `cat server_update_list`      #This loops through the list of IPs
+do
+widget=`ssh -f root@$server 'facter -p widget'`   #Remotely retrieve facter value from machine
+
+if [[ -n $widget && $? -eq 0 ]]; then   #Make sure there is a value for widget and ssh fork exited clean
+
+sed -i "s/widget_type\ .*/widget_type $widget/g" /root/template.file   #switch widget_type with acquired value
+scp template.file root@$server:/etc/widgetfile/
+success=$(expr $success + 1)  #Increase successful counter
+
+else                #Either widget was empty or there was an error with ssh (both instances constitute a failure to update)
+
+fail=$(expr $fail + 1)      #Increase failed counter
+echo $server >> /tmp/failed_servers.tmp
+
+fi
+
+done
+
+echo "Number of successful attempts: ${success}"
+echo "Number of failed attempts: ${fail}"
+echo "Here is a list of the server(s) that failed:"
+cat /tmp/failed_servers.tmp
+rm -f /tmp/failed_servers.tmp
